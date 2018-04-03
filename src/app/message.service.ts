19a48431b66b859948135d423fa295b97bc55670
Erald Bulku <erald.b@scopicsoftware.com>
2018-04-03 17:04:53 +0200

    Http services

--- src/app/message.service.ts
+++ src/app/message.service.ts
@@ -2,7 +2,6 @@ import { Injectable } from '@angular/core';
 
 @Injectable()
 export class MessageService {
-
   messages: string[] = [];
 
   add(message: string) {
@@ -12,5 +11,4 @@ export class MessageService {
   clear() {
     this.messages = [];
   }
-
 }
